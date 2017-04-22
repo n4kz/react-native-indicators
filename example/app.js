6aed4ae5acdf9d15670e15760b50f865f2831ca4
n4kz <n4kz@n4kz.com>
2017-04-22 13:37:10 +0300

    Added example

---
+++ example/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { AppRegistry, StatusBar, View, Platform } from 'react-native';
+import { DotIndicator } from 'react-native-indicators';
+
+Platform.select({
+  ios: () => StatusBar.setBarStyle('light-content'),
+  android: () => StatusBar.setBackgroundColor('#263238'),
+})();
+
+export default function init() {
+  class Example extends Component {
+    render() {
+      return (
+        <View style={{ flex: 1 }}>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <DotIndicator color='#7B1FA2' />
+            </View>
+          </View>
+
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
+              <DotIndicator count={3} color='#1976D2' animationDuration={800} />
+            </View>
+          </View>
+        </View>
+      );
+    }
+  }
+
+  AppRegistry.registerComponent('example', () => Example);
+}
---
+++ example/index.android.js
@@ -0,0 +1,3 @@
+import init from './app';
+
+init();
