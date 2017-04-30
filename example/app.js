be4455ea1d8db423022a4901afaceae26f6be6ab
n4kz <n4kz@n4kz.com>
2017-04-30 11:26:30 +0300

    Updated example

--- example/app.js
+++ example/app.js
@@ -20,6 +20,26 @@ export default function init() {
 
           <View style={{ flex: 1, flexDirection: 'row' }}>
             <View style={{ flex: 1 }}>
+              <DotIndicator
+                count={3}
+                size={20}
+                color='#1976D2'
+                animationDuration={800}
+              />
+            </View>
+            <View style={{ flex: 1 }}>
+              <DotIndicator
+                count={3}
+                size={20}
+                color='#303F9F'
+                animationDuration={800}
+                animationDirection='backward'
+              />
+            </View>
+          </View>
+
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ flex: 1 }}>
               <WaveIndicator color='#0097A7' />
             </View>
 
@@ -45,24 +65,6 @@ export default function init() {
           <View style={{ flex: 1, flexDirection: 'row' }}>
             <View style={{ flex: 1 }}>
               <BarIndicator color='#FF6F00' count={5} />
-            </View>
-          </View>
-
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            <View style={{ flex: 1 }}>
-              <DotIndicator
-                count={3}
-                color='#1976D2'
-                animationDuration={800}
-              />
-            </View>
-            <View style={{ flex: 1 }}>
-              <DotIndicator
-                count={3}
-                color='#303F9F'
-                animationDuration={800}
-                animationDirection='backward'
-              />
             </View>
           </View>
         </View>
