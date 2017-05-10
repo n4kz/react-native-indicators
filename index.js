fded2197b1fb0fd9eb8d9b50ce8ee34b048cebd3
n4kz <n4kz@n4kz.com>
2017-05-10 22:42:26 +0300

    Implemented PacmanIndicator

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import DotIndicator from './src/components/dot-indicator';
 import BarIndicator from './src/components/bar-indicator';
 import BallIndicator from './src/components/ball-indicator';
 import SkypeIndicator from './src/components/skype-indicator';
+import PacmanIndicator from './src/components/pacman-indicator';
 import PulseIndicator from './src/components/pulse-indicator';
 import WaveIndicator from './src/components/wave-indicator';
 import UIActivityIndicator from './src/components/ui-activity-indicator';
@@ -13,6 +14,7 @@ export {
   BarIndicator,
   BallIndicator,
   SkypeIndicator,
+  PacmanIndicator,
   PulseIndicator,
   WaveIndicator,
   UIActivityIndicator,
---
+++ src/components/pacman-indicator/index.js
@@ -0,0 +1,125 @@
+import React, { PureComponent, PropTypes } from 'react';
+import { View, Animated, Easing } from 'react-native';
+
+import Indicator from '../indicator';
+import styles from './styles';
+
+export default class PacmanIndicator extends PureComponent {
+  static defaultProps = {
+    animationDuration: 600,
+
+    color: 'rgb(0, 0, 0)',
+    size: 48,
+  };
+
+  static propTypes = {
+    ...Indicator.propTypes,
+
+    color: PropTypes.string,
+    size: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.renderComponent = this.renderComponent.bind(this);
+  }
+
+  renderBlock({ index, count, progress }) {
+    let { size, color: backgroundColor } = this.props;
+
+    let style = {
+      position: 'absolute',
+      top: size / 2 - size / 16,
+      left: size / 2 + size / 16 + (index - 2) * size / 4,
+      width: size / 8,
+      height: size / 8,
+      borderRadius: size / 16,
+      backgroundColor,
+      transform: [{
+        translateX: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, -size / 4],
+        }),
+      }],
+    };
+
+    if (count === index + 1) {
+      style.opacity = progress.interpolate({
+        inputRange: [0, 0.67, 1],
+        outputRange: [0, 1, 1],
+      });
+    }
+
+    return (
+      <Animated.View style={style} key={index} />
+    );
+  }
+
+  renderComponent({ index, count, progress }) {
+    let { size, color: backgroundColor } = this.props;
+
+    if (index > 1) {
+      return this.renderBlock({ index, count, progress });
+    }
+
+    let pacmanStyle = {
+      position: 'absolute',
+
+      top: size / 4,
+      left: 0,
+      width: size / 2,
+      height: size / 2,
+
+      transform: [{
+        rotate: progress.interpolate({
+          inputRange: [0, 0.67, 1],
+          outputRange: index?
+            ['0deg',  '45deg', '0deg']:
+            ['0deg', '-45deg', '0deg'],
+        }),
+      }],
+    };
+
+    let containerStyle = {
+      width: size / 2,
+      height: size / 4,
+      overflow: 'hidden',
+    };
+
+    let style = {
+      width: size / 2,
+      height: size / 2,
+      borderRadius: size / 4,
+      backgroundColor,
+    };
+
+    if (index) {
+      containerStyle.top = size / 4;
+      style.top = -size / 4;
+    }
+
+    return (
+      <Animated.View style={pacmanStyle} key={index}>
+        <View style={containerStyle}>
+          <Animated.View style={style} />
+        </View>
+      </Animated.View>
+    );
+  }
+
+  render() {
+    let { style, size, ...props } = this.props;
+
+    return (
+      <View style={[styles.container, style]}>
+        <Indicator
+          style={{ width: size * 1.25, height: size }}
+          renderComponent={this.renderComponent}
+          {...props}
+          count={5}
+        />
+      </View>
+    );
+  }
+}
