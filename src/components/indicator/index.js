c263d48283c1fc2d5bdbcc27009ee3dabf7b1622
n4kz <n4kz@n4kz.com>
2017-05-16 21:26:57 +0300

    Use PropTypes from prop-types package

--- src/components/indicator/index.js
+++ src/components/indicator/index.js
@@ -1,4 +1,5 @@
-import React, { PureComponent, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
 import { Animated, Easing } from 'react-native';
 
 export default class Indicator extends PureComponent {
