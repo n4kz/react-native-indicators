bb80380b0ff7e35356a7c8ffb6e98dc5af67fc18
n4kz <n4kz@n4kz.com>
2017-07-03 14:33:43 +0300

    Implemented MaterialIndicator

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import Indicator from './src/components/indicator';
-import DotIndicator from './src/components/dot-indicator';
 import BarIndicator from './src/components/bar-indicator';
 import BallIndicator from './src/components/ball-indicator';
-import SkypeIndicator from './src/components/skype-indicator';
+import DotIndicator from './src/components/dot-indicator';
+import MaterialIndicator from './src/components/material-indicator';
 import PacmanIndicator from './src/components/pacman-indicator';
 import PulseIndicator from './src/components/pulse-indicator';
-import WaveIndicator from './src/components/wave-indicator';
+import SkypeIndicator from './src/components/skype-indicator';
 import UIActivityIndicator from './src/components/ui-activity-indicator';
+import WaveIndicator from './src/components/wave-indicator';
 
 export {
   Indicator,
-  DotIndicator,
   BarIndicator,
   BallIndicator,
-  SkypeIndicator,
+  DotIndicator,
+  MaterialIndicator,
   PacmanIndicator,
   PulseIndicator,
-  WaveIndicator,
+  SkypeIndicator,
   UIActivityIndicator,
+  WaveIndicator,
 };
---
+++ src/components/material-indicator/index.js
@@ -0,0 +1,124 @@
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { View, Animated, Easing } from 'react-native';
+
+import Indicator from '../indicator';
+import styles from './styles';
+
+export default class MaterialIndicator extends PureComponent {
+  static defaultProps = {
+    animationDuration: 2400,
+
+    color: 'rgb(0, 0, 0)',
+    size: 40,
+  };
+
+  static propTypes = {
+    ...Indicator.propTypes,
+
+    color: PropTypes.string,
+    size: PropTypes.number,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.renderComponent = this.renderComponent.bind(this);
+  }
+
+  renderComponent({ index, count, progress }) {
+    let { size, color, animationDuration } = this.props;
+
+    let frames = 60 * animationDuration / 1000;
+    let easing = Easing.bezier(0.4, 0.0, 0.7, 1.0);
+
+    let inputRange = Array
+      .from(new Array(frames), (undefined, frameIndex) => frameIndex / (frames - 1));
+
+    let outputRange = Array
+      .from(new Array(frames), (undefined, frameIndex) => {
+        let progress = 2 * frameIndex / (frames - 1);
+        let rotation = index?
+          +(360 - 15):
+          -(180 - 15);
+
+        if (progress > 1.0) {
+          progress = 2.0 - progress;
+        }
+
+        let direction = index?
+          -1:
+          +1;
+
+        return (direction * (180 - 30) * easing(progress) + rotation) + 'deg';
+      });
+
+    let layerStyle = {
+      width: size,
+      height: size,
+      transform: [{
+        rotate: progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [(0 + 30 + 15) + 'deg', (2 * 360 + 30 + 15) + 'deg'],
+        }),
+      }],
+    };
+
+    let viewportStyle = {
+      width: size,
+      height: size,
+      transform: [{
+        translateY: index? -size / 2 : 0,
+      }, {
+        rotate: progress.interpolate({ inputRange, outputRange }),
+      }],
+    };
+
+    let containerStyle = {
+      width: size,
+      height: size / 2,
+      overflow: 'hidden',
+    };
+
+    let offsetStyle = index?
+      { top: size / 2 }:
+      null;
+
+    let lineStyle = {
+      width: size,
+      height: size,
+      borderColor: color,
+      borderWidth: size / 10,
+      borderRadius: size / 2,
+    };
+
+    return (
+      <Animated.View style={styles.layer} {...{ key: index }}>
+        <Animated.View style={layerStyle}>
+          <Animated.View style={[containerStyle, offsetStyle]}>
+            <Animated.View style={viewportStyle}>
+              <Animated.View style={containerStyle}>
+                <Animated.View style={lineStyle} />
+              </Animated.View>
+            </Animated.View>
+          </Animated.View>
+        </Animated.View>
+      </Animated.View>
+    );
+  }
+
+  render() {
+    let { style, size: width, size: height, ...props } = this.props;
+
+    return (
+      <View style={[styles.container, style]}>
+        <Indicator
+          style={{ width, height }}
+          renderComponent={this.renderComponent}
+          {...props}
+          count={2}
+        />
+      </View>
+    );
+  }
+}
